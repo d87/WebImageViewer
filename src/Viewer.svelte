a9e23cb86ed6a9d9564c03069eeaf68c4dcc9437
d87 <d87d87@gmail.com>
2021-01-31 19:30:29 +0700

    fullscreen

--- src/Viewer.svelte
+++ src/Viewer.svelte
@@ -7,31 +7,47 @@
     export let onPrev: (event: Event) => void
     export let onNext: (event: Event) => void
 
+    let isFullscreen = false
+    let fullscreenRoot: HTMLElement
+
+    const handleFullscreenEnter = () => {
+        if (fullscreenRoot && !document.fullscreenElement) {
+            fullscreenRoot.requestFullscreen()
+        }
+    }
+
+    const handleFullscreenExit = () => {
+        document.exitFullscreen()
+    }
+
     $: imagePath = playlist[index]
     $: prefetchPath = playlist[index+1]
     $: prefetchPath2 = playlist[index+2]
 </script>
 
-<div class="fixed bottom-0 left-0 top-0 right-0 z-10">
-    <div class="fixed bottom-0 left-0 top-0 right-0 bg-black opacity-80 "/>
+<div class="fixed bottom-0 left-0 top-0 right-0 z-10 w-full h-full">
+    <div class="absolute bottom-0 left-0 top-0 right-0 bg-black opacity-90"/>
 
-    <!-- <HotKeys keyMap={keyMap} handlers={keybindHandlers} allowChanges={true}> -->
-        <div class="relative z-20">
-            <!-- <a class="px-4" on:click={fullscreenHandle.enter}>Fullscreen</a> -->
-            <a class="px-4" on:click={onClose} href=".">Exit</a>
+    <div class="relative z-20  w-full h-full"  on:click|preventDefault={onClose}>
+        <div class="text-center">
+            <a class="px-4 py-2" href=" " on:click|stopPropagation|preventDefault={handleFullscreenEnter}>Fullscreen</a>
+            <!-- <a class="px-4 py-2" href=" " on:click|preventDefault={onClose}>Exit</a> -->
         </div>
 
-        <!-- <FullScreen handle={fullscreenHandle}> -->
-        <div class="absolute top-0 left-0 flex justify-center items-center w-full h-full">
+
+        <div bind:this={fullscreenRoot} class="grid place-items-center content-center h-full w-full">
             <DynamicMedia  path={imagePath}/>
             <PrefetchMedia path={prefetchPath}/>
             <PrefetchMedia path={prefetchPath2}/>
-            <div on:click={onPrev} class="fixed z-40 top-0 left-0 w-10 h-full bg-black opacity-10"></div>
-            <div on:click={onNext} class="fixed z-40 top-0 right-0 w-10 h-full bg-black opacity-10"></div>
+            <buttton on:click|stopPropagation={onPrev} class="fixed z-40 top-0 cursor-pointer left-0 w-10 h-full bg-black opacity-0 lg:hover:opacity-10"></buttton>
+            <buttton on:click|stopPropagation={onNext} class="fixed z-40 top-0 cursor-pointer right-0 w-10 h-full bg-black opacity-0 lg:hover:opacity-10"></buttton>
         </div>
-        <!-- </FullScreen> -->
-    <!-- </HotKeys> -->
+    </div>
 </div>
 
 <style>
+    a {
+        color: #aaa;
+        text-decoration: none;
+    }
 </style>
